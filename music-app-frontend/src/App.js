59a382871710de2b2b0fd66c950b89b239daa02d
Mitchell Diaz <mitchelldiazlmt@gmail.com>
2022-12-12 12:15:19 -0500

    initial project setup

---
+++ music-app-frontend/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+// import { useState, useEffect } from "react"
+import { Link, Routes, Route } from "react-router-dom"; 
+import HomePage from './pages/HomePage';
+import SignupForm from "./pages/SignupForm"
+import { UserProvider } from "./contexts/UserContext";
+
+
+function App() {
+
+  // const {logout } = useContext(UserContext);
+
+    return (
+        <div className="App">
+            <UserProvider>
+
+                <HomePage/>
+
+                <Routes>
+                    <Route path="/auth/login" element={<SignupForm />} />
+                    <Route path="/auth/signup" element={<SignupOrLogin />} />
+                </Routes>
+
+            </UserProvider>
+
+        </div>
+    );
+}
+
+export default App;
